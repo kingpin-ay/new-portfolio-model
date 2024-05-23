053c0917f7568ce0f8514a82b197ec02158fde5e
kingpin-ay <ayushmondal001@gmail.com>
2024-05-24 01:33:37 +0530

    feat: added font awsome icons
    added font awsome icons and also added some extra things on the tech use
    page

---
+++ app/lib/data/techStack.ts
@@ -0,0 +1,31 @@
+
+// import {
+//   home,
+//   logoStackoverflow,
+//   planet,
+//   school,
+//   people,
+//   hammer,
+// } from "ionicons/icons";
+
+import { faNeos, faLinux, faDocker } from '@fortawesome/free-brands-svg-icons'
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export const everyDayLifeTools = [
+  {
+    icon: faNeos,
+    name: "Neovim",
+    context: "Editor"
+  },
+  {
+    name: "Arch Linux",
+    icon: faLinux,
+    context: "Operating System"
+  },
+  {
+    name: "Docker",
+    icon: faDocker,
+    context: "Testing"
+  },
+]
+
